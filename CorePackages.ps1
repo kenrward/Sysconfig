f94036017bf15924c79c4afbebff6e07bb063c9a
kenrward <kenrward@gmail.com>
2015-06-25 18:19:19 +0000

    core packages via Choco

---
+++ CorePackages.ps1
@@ -0,0 +1,6 @@
+﻿choco install notepadplusplus latest -y
+choco install googlechrome latest -y
+choco install lastpass -y
+choco install lastpass-for-applications -y
+choco install fiddler -y
+choco install visualstudiocommunity2013 -y
