9b2f36bad73b8d55dd6946a068cbe4a8c363d5fc
kenrward <kenrward@gmail.com>
2017-09-29 07:17:22 -0400

    Python Update

--- CorePackages.ps1
+++ CorePackages.ps1
@@ -1,6 +1,8 @@
-﻿choco install notepadplusplus latest -y
+choco install notepadplusplus latest -y
 choco install googlechrome latest -y
 choco install lastpass -y
 choco install lastpass-for-applications -y
 choco install fiddler -y
-choco install visualstudiocommunity2013 -y
+# choco install visualstudiocommunity2013 -y
+choco install python2 -y
+choco install pip -y
